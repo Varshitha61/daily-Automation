d2a854a3f4aca85b2d19ffe33b4e4717e8dc288f
Varshitha Somashekar <varshithasomashekar22@gmail.com>
2026-07-08 10:10:34 +0000

    chore: automate daily coding bot run [skip ci]

--- solutions/leetcode/2026-07-08_concatenate_non-zero_digits_and_multiply_by_sum_ii.cpp
+++ solutions/leetcode/2026-07-08_concatenate_non-zero_digits_and_multiply_by_sum_ii.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <numeric>
 
 using namespace std;
 
 const int MOD = 1e9 + 7;
 
-int sumOfDigits(int x) {
+int getSum(int x) {
     int sum = 0;
     while (x > 0) {
         sum += x % 10;
@@ -16,27 +15,27 @@ int sumOfDigits(int x) {
     return sum;
 }
 
-int concatNonZeroDigitsAndMultiplyBySum(const string& s, const vector<vector<int>>& queries) {
-    int result = 0;
+int getConcatenatedNumber(const string& s, int left, int right) {
+    int concatenatedNumber = 0;
+    for (int i = left; i <= right; i++) {
+        if (s[i] != '0') {
+            concatenatedNumber = concatenatedNumber * 10 + (s[i] - '0');
+        }
+    }
+    return concatenatedNumber;
+}
+
+vector<int> processQueries(const string& s, const vector<vector<int>>& queries) {
+    vector<int> answers;
     for (const auto& query : queries) {
         int left = query[0];
         int right = query[1];
-        string substring = s.substr(left, right - left + 1);
-        string nonZeroDigits;
-        for (char c : substring) {
-            if (c != '0') {
-                nonZeroDigits += c;
-            }
-        }
-        if (nonZeroDigits.empty()) {
-            result = 0;
-        } else {
-            int x = stoi(nonZeroDigits);
-            int sum = sumOfDigits(x);
-            result = (static_cast<long long>(x) * sum) % MOD;
-        }
+        int concatenatedNumber = getConcatenatedNumber(s, left, right);
+        int sum = getSum(concatenatedNumber);
+        long long answer = (long long)concatenatedNumber * sum % MOD;
+        answers.push_back(answer);
     }
-    return result;
+    return answers;
 }
 
 int main() {
@@ -44,28 +43,10 @@ int main() {
     int q;
     cin >> s >> q;
     vector<vector<int>> queries(q, vector<int>(2));
-    for (int i = 0; i < q; ++i) {
+    for (int i = 0; i < q; i++) {
         cin >> queries[i][0] >> queries[i][1];
     }
-    vector<int> answers;
-    for (const auto& query : queries) {
-        int left = query[0];
-        int right = query[1];
-        string substring = s.substr(left, right - left + 1);
-        string nonZeroDigits;
-        for (char c : substring) {
-            if (c != '0') {
-                nonZeroDigits += c;
-            }
-        }
-        if (nonZeroDigits.empty()) {
-            answers.push_back(0);
-        } else {
-            int x = stoi(nonZeroDigits);
-            int sum = sumOfDigits(x);
-            answers.push_back((static_cast<long long>(x) * sum) % MOD);
-        }
-    }
+    vector<int> answers = processQueries(s, queries);
     for (int answer : answers) {
         cout << answer << " ";
     }
