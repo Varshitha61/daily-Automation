b72d2486d2fefbf735881dc94fb92c84e1c60faf
Varshitha Somashekar <varshithasomashekar22@gmail.com>
2026-08-01 09:52:27 +0000

    chore: automate daily coding bot run [skip ci]

---
+++ solutions/leetcode/2026-08-01_predict_the_winner.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        
+        for (int length = 1; length < n; length++) {
+            for (int i = 0; i < n - length; i++) {
+                int j = i + length;
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] >= 0;
+    }
+};
